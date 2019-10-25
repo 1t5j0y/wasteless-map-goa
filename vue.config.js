af5c327f6be7616a2f86c0ad7622c07e12efaa99
Joy <itsjoy@gmail.com>
2019-10-25 15:56:44 +0530

    added s3 deploy plugin

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,22 @@
-module.exports = {publicPath: ''};
+module.exports = {
+  publicPath: '',
+
+  pluginOptions: {
+    s3Deploy: {
+      registry: undefined,
+      awsProfile: 'default',
+      region: 'ap-south-1',
+      bucket: 'goa-waste-eco-map',
+      createBucket: false,
+      staticHosting: false,
+      assetPath: 'dist',
+      assetMatch: '**',
+      deployPath: '/webapp',
+      acl: 'public-read',
+      pwa: false,
+      enableCloudfront: false,
+      uploadConcurrency: 5,
+      pluginVersion: '3.0.0'
+    }
+  }
+};
